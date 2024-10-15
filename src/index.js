4a6d4265619948c988e526f0ff8db2a43ecc8a8c
unknown <namtrungle20@gmail.com>
2024-10-15 11:09:14 +0700

    them file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './pages/users/homePage';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
+import './styles/styles.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HomePage/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+   <BrowserRouter>
+   <RouterCustom />
+   </BrowserRouter>
+);
---
+++ src/pages/users/theme/header/index.js
@@ -0,0 +1,12 @@
+import { memo } from "react";
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header-top">
+      <div className="container">Header</div>
+    </div>
+  );
+};
+
+export default memo(Header);
