eeeb8bda9467344a4827acd0465c5140aa75b913
unknown <namtrungle20@gmail.com>
2024-10-17 12:33:13 +0700

    Them icon

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,10 +1,36 @@
 import { memo } from "react";
 import "./styles.scss";
-
+// import { logo } from "/src/assets/images/logo_tempest_gaming.png";
+import { FaFacebook } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { MdAccountCircle } from "react-icons/md";
 const Header = () => {
   return (
     <div className="header-top">
-      <div className="container">Header</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Trái</div>
+          <div className="col-6 header-top-right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <FaFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaGoogle />
+                </Link>
+              </li>
+              <Link to={""}>
+                <MdAccountCircle />
+              </Link>
+              <span>Đăng Nhập</span>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
