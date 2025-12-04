3879ac99933299b062ca2b204836d4c30d6e5b9c
namtrungle20 <121472249+namtrungle20@users.noreply.github.com>
2025-12-04 18:39:51 +0700

    update 04-12-2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import AdminWelcome from "./pages/admin/AdminWelcome.jsx";
-import UserWelcome from "./pages/UserWelcome.jsx";
+import Login from "./scenes/Auth/Login.jsx";
+import AdminDashboard from "./scenes/Admin/Dashboard.jsx";
+import UserDashboard from "./scenes/User/UserDashboard.jsx";
 
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<AdminWelcome />} />
-        <Route path="/user" element={<UserWelcome />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/user" element={<UserDashboard />} />
       </Routes>
     </Router>
   );
--- src/scenes/Admin/Dashboard.jsx
+++ src/scenes/Admin/Dashboard.jsx
@@ -1,9 +1,8 @@
-// UserWelcome.jsx
-export default function UserWelcome() {
+export default function Dashboard() {
   const email = localStorage.getItem("email");
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>🎉 Chào mừng User 🎉</h2>
+      <h2>🔑 Chào mừng Admin</h2>
       <p>Bạn đã đăng nhập thành công với email: {email}</p>
     </div>
   );
