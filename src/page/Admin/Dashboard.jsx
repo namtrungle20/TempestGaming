ad4e43e25130eb55c6a0d9ef84e98c45430cf2f5
namtrungle20 <121472249+namtrungle20@users.noreply.github.com>
2025-12-17 19:28:53 +0700

    chỉnh sửa mã hóa cho backend 17/12/2025

--- src/page/Admin/Dashboard.jsx
+++ src/page/Admin/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Dashboard() {
+function Dashboard() {
   return (
     <div>
-      <p1>Welcome To Admin</p1>
+      
     </div>
   )
 }
+
+export default Dashboard
